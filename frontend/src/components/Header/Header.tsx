6a2b4b6b68c8d223656624da85b3328b0f224585
Christa <117929321+ch-risto@users.noreply.github.com>
2025-09-21 14:30:34 +0300

    Added elements for header

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import chLogo from '../../assets/ch_.png';
+import { ButtonContainer, HeaderContainer, HeaderTitle, StyledHeaderLink } from './header.styles';
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <StyledHeaderLink to='/'>
+        <img src={chLogo} className='logo ch' alt='ch-logo christa eloranta' />
+
+        <HeaderTitle>Christa Eloranta</HeaderTitle>
+      </StyledHeaderLink>
+      <ButtonContainer>
+        <p>Minusta</p>
+        <p>cv</p>
+        <p>Töitäni</p>
+      </ButtonContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
