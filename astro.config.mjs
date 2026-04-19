034e5362b01ff593ce0c2cb0e257f6b1c2786438
Christa <117929321+ch-risto@users.noreply.github.com>
2026-04-19 15:58:28 +0300

    added icons, improved ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
